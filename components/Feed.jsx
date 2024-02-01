e0949830d1cbcc95f2f0906f64189da577976de4
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-02-02 00:13:36 +0530

    first commit

---
+++ components/Feed.jsx
@@ -0,0 +1,92 @@
+'use client';
+
+import {useState,useEffect} from 'react'
+import PromptCard from './PromptCard'
+
+const PromptCardList=({data,handleTagClick})=>{
+  return(
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+        key={post._id}
+        post={post}
+        handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+ const [searchText, setSearchText] = useState('');
+ const [searchTimeOut, setSearchTimeOut] = useState(null);
+ const [searchedResults, setSearchedResults] = useState([]);
+ const [posts,setPosts]=useState([]);
+
+
+ const fetchPosts = async () => {
+  const response = await fetch("/api/prompt");
+  const data = await response.json();
+
+  setPosts(data);
+};
+
+useEffect(() => {
+  fetchPosts();
+}, []);
+
+ const filterPrompts=(searchtext)=>{
+  const regex=new RegExp(searchtext,"i");
+  return posts.filter((item)=>
+    regex.test(item.creator.username)||
+    regex.test(item.tag)||
+    regex.test(item.prompt)
+  )
+ };
+
+ const handleSearchChange = (e) =>{
+  clearTimeout(searchTimeOut);
+    setSearchText(e.target.value);
+  setSearchTimeOut(
+    setTimeout(()=>{
+      const searchedResult=filterPrompts(e.target.value);
+      setSearchedResults(searchedResult);
+    },500)
+   );
+   
+ };
+
+ 
+
+const handleTagClick=(tagName)=>{
+  setSearchText(tagName);
+  const searchResult = filterPrompts(tagName);
+  setSearchedResults(searchResult);
+};
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+        type='text'
+        placeholder='Search for prompts or tags or username'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className='search_input peer'
+        />
+      </form>
+
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
+    </section>
+  )
+}
+
+export default Feed
